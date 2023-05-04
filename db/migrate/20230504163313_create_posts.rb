032d8df4374fe2c316005ac3ff8c854ed4e91903
dfsalam <dfsalam@gmail.com>
2023-05-04 12:05:36 -0500

    Create likes table migration.

--- db/migrate/20230504163313_create_posts.rb
+++ db/migrate/20230504163313_create_posts.rb
@@ -6,6 +6,8 @@ class CreatePosts < ActiveRecord::Migration[7.0]
       t.text :text
       t.datetime :created_at
       t.datetime :updated_at
+      t.integer :comments_counter
+      t.integer :likes_counter
       self.primary_key = 'postId'
       t.timestamps
     end
--- db/migrate/20230504170211_create_likes.rb
+++ db/migrate/20230504170211_create_likes.rb
@@ -1,12 +1,11 @@
-class CreateComments < ActiveRecord::Migration[7.0]
+class CreateLikes < ActiveRecord::Migration[7.0]
   def change
-    create_table :comments do |t|
+    create_table :likes do |t|
       t.references :author, index: true, foreign_key: {to_table: :users, primary_key: :authorId}
       t.references :post, index: true, foreign_key: {to_table: :posts, primary_key: :postId}
-      t.text :text
       t.datetime :created_at
-      t.datetime :updated_at      
-      self.primary_key = 'commentId'
+      t.datetime :updated_at
+      self.primary_key = 'likeId'
       t.timestamps
     end
   end
