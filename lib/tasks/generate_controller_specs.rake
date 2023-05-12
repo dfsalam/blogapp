dd06e08cc34cef39e513a0fc746696781c3c0efb
dfsalam <dfsalam@gmail.com>
2023-05-11 22:17:15 -0500

    Add users_controller_spec

--- lib/tasks/generate_controller_specs.rake
+++ lib/tasks/generate_controller_specs.rake
@@ -1,12 +1,12 @@
 namespace :spec do
-  desc "Generate specs for all controllers"
+  desc 'Generate specs for all controllers'
   task generate_controller_specs: :environment do
     Rails.application.eager_load!
-    
+
     controllers = ApplicationController.descendants.reject do |controller|
-      controller.name.include?("Rails")
+      controller.name.include?('Rails')
     end
-    
+
     controllers.each do |controller|
       system("rails generate rspec:controller #{controller.name.underscore}")
     end
---
+++ spec/requests/users_controller_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'UsersControllers', type: :request do
+  describe 'GET /index' do
+    it 'Returns http success' do
+      get '/users'
+      expect(response.status).to eq(200)
+    end
+  end
+  describe 'GET /show' do
+    first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+                             bio: 'Teacher from Mexico.')
+    first_user_id = first_user.id
+    it 'Returns http success' do
+      get "/users/#{first_user_id}"
+      expect(response.status).to eq(200)
+    end
+    it 'Returns not found' do
+      expect { get "/users/789" }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
+end
